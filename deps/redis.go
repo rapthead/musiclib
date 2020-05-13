c8b64e53794c51053018ad1aafae8a220318b1d8
Павел Грибанов <rapthead@gmail.com>
2020-05-13 12:15:50 +0500

    remove debug print in redis instantiating

--- deps/redis.go
+++ deps/redis.go
@@ -1,14 +1,11 @@
 package deps
 
 import (
-	"fmt"
-
 	"github.com/go-redis/redis/v7"
 	"github.com/rapthead/musiclib/config"
 )
 
 func makeRedis() *redis.Client {
 	conf := config.Config
-	fmt.Println(conf.RedisOpts)
 	return redis.NewClient(conf.RedisOpts)
 }
