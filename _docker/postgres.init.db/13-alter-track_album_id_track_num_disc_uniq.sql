c73679ed12baf72e6f3238e59ca09f60270ddf48
Павел Грибанов <rapthead@gmail.com>
2020-09-10 14:17:47 +0500

    fix track_album_id_track_num_disc_uniq

--- _docker/postgres.init.db/13-alter-track_album_id_track_num_disc_uniq.sql
+++ _docker/postgres.init.db/13-alter-track_album_id_track_num_disc_uniq.sql
@@ -3,5 +3,5 @@ BEGIN;
         DROP CONSTRAINT track_album_id_track_num_disc_uniq;
 
     CREATE UNIQUE INDEX track_album_id_track_num_disc_uniq
-        ON track (album_id, track_num, disc) WHERE (track.state != 'draft');
+        ON track (album_id, track_num, disc) WHERE (track.album_state != 'draft');
 COMMIT;
